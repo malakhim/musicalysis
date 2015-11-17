e9a56c6c86909b686871e687778006d0ecdd5ad3
Mary-Anne Ng <maryanne.nghc@gmail.com>
2015-11-17 18:37:13 +1100

    front page css, image

--- application/views/themes/default/templates/default.php
+++ application/views/themes/default/templates/default.php
@@ -2,7 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');?>
 <!DOCTYPE html>
 <html lang="en">
- 
     <head>
     	<meta charset="utf-8">
         <title><?php echo $title; ?></title>
