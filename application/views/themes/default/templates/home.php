1240d8f34e949d8f2bdb433dc5904dacfccf508a
Mary-Anne Ng <maryanne.nghc@gmail.com>
2015-11-18 11:04:09 +1100

    navbar, front page tweaks
    enlarged navbar font, enlarged navbar, search bar, tweaked CTA buttons
    and margins of main text

--- application/views/themes/default/templates/home.php
+++ application/views/themes/default/templates/home.php
@@ -6,7 +6,7 @@
     <div class="main_text">Listen to classical music you know and love with <div class="bold_text">real-time programme notes.</div></div>
 
     <div class="main_CTA">
-    	<a href="/"><div class="CTA_btn left">Listen</div></a>
-    	<a href="/"><div class="CTA_btn right">Learn More</div></a>
+    	<a href="/pieces"><div class="CTA_btn left">Listen</div></a>
+    	<a href="/about"><div class="CTA_btn right">Learn More</div></a>
     </div>
 </div>
