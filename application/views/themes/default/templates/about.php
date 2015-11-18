9bf6573923fe67385b0c4feef15d56ce9fdf27b6
Mary-Anne Ng <maryanne.nghc@gmail.com>
2015-11-18 21:17:25 +1100

    bunch of font size change

--- application/views/themes/default/templates/about.php
+++ application/views/themes/default/templates/about.php
@@ -1,7 +1,7 @@
 <input type="hidden" name="title" value="About"/>
 
 <div class="main_container">
-	<div class="red_highlight_text"><div class="devonshire_heading">Classical music</div> made accessible and easily understandable for anyone.</div>
+	<div class="devonshire_heading">Classical music</div><div class="red_highlight_text">made accessible and easily understandable for anyone.</div>
 	<div class="body_text">Musicalysis aims to challenge the stereotypes that Classical music is complicated and boring.<br>
 	Were you always curious about classical music but felt too intimidated by the massive amount of jargon and theory? Or maybe you recognize famous tunes but not so much the rest of the piece, and would like to change that. Or perhaps you love listening to classical music and would like to delve more into its traditions.</div>
 	<div class="red_highlight_text">We love classical music, and would like to share this love with you.</div>
