5519f3f66cfaabedf45e5cf490ba5a2b07d752b5
Mary-Anne Ng <maryanne.nghc@gmail.com>
2015-11-19 14:34:26 +1100

    button changes

--- application/views/themes/default/templates/contact.php
+++ application/views/themes/default/templates/contact.php
@@ -1,1 +1,12 @@
 <input type="hidden" name="title" value="Contact"/>
+<form method="post" action="/contact" class="contact_form">
+	<div class="form-group">
+		<label for="contact_name">Name:</label>
+		<input type="text" name="name" id="contact_name" placeholder="Name" class="form-control"/>
+	</div>
+	<div class="form-group">
+		<label for="contact_email">Email:</label>
+		<input type="email" name="email" id="contact_email" placeholder="Email" class="form-control"/>
+	</div>
+	<button type="submit" id="submit_button" class="btn btn_default">Submit</button>
+</form>
