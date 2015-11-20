f89c2480e414d874e39b4a5b7afa517f4fbfd2f4
malakhim <bryan@bryonics.com>
2015-11-20 13:19:52 +1100

    Sigh

--- application/config/config.php
+++ application/config/config.php
@@ -28,7 +28,7 @@ if(strpos($_SERVER['SERVER_NAME'],'bw') !== FALSE)
 elseif(strpos($_SERVER['SERVER_NAME'],'ma') !== FALSE)
 	$config['base_url'] = 'http://ma.musicalysis.com';
 elseif(strpos($_SERVER['SERVER_NAME'],'musicalysis.brystore') !== FALSE){
-	$config['base_url'] = 'http://musicalysis.brystore.com/';
+	$config['base_url'] = 'http://musicalysis.brystore.com';
 }
 
 /*
