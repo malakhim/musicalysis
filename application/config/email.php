febb475d127965a45a04542a45dc0177f9645996
malakhim <bryan@bryonics.com>
2015-11-21 17:38:37 +1100

    Code for captcha

--- application/config/email.php
+++ application/config/email.php
@@ -1,11 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['protocol'] = 'smtp';
-// $config['mailpath'] = '/usr/sbin/sendmail';
-$config['smtp_host'] = 'gator4107.hostgator.com';
-$config['smtp_user'] = 'contact@musicalysis.com';
-$config['smtp_pass'] = 'mps133kkWin43BoCTqTh';
-$config['smtp_port'] = 465;
+$config['protocol'] = 'sendmail';
 $config['charset'] = 'iso-8859-1';
 $config['wordwrap'] = TRUE;
