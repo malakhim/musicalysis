47a602a98d1ab586069cbac7dafb461f4254a65f
malakhim <bryan@bryonics.com>
2015-11-21 11:42:48 +1100

    Contact form language variable substitution

--- application/views/themes/default/templates/contact.php
+++ application/views/themes/default/templates/contact.php
@@ -1,23 +1,24 @@
 <input type="hidden" name="title" value="Contact"/>
 <div class="contact_main_container">
 	<div class="red_highlight_text">
-		Have something to say? We’d love to hear from you.
+		<?php echo $this->lang->line('contact_page_title')?>
 	</div>
-	<div class="about_body_text">We welcome feedback, piece suggestions, general queries, and contributions! Do leave us a note and we will get back to you as soon as possible.
+	<div class="about_body_text">
+		<?php echo $this->lang->line('contact_page_text')?>
 	</div>
 </div>
 <form method="post" action="/contact" class="contact_form">
 	<div class="form-group small-form-group">
-		<label for="contact_name">Name:</label>
-		<input type="text" name="name" id="contact_name" placeholder="Name" class="form-control"/>
+		<label for="contact_name"><?php echo ucfirst($this->lang->line('name'))?>:</label>
+		<input type="text" name="name" id="contact_name" placeholder="<?php echo ucfirst($this->lang->line('name'))?>" class="form-control"/>
 	</div>
 	<div class="form-group small-form-group">
-		<label for="contact_email">Email:</label>
-		<input type="email" name="email" id="contact_email" placeholder="Email" class="form-control"/>
+		<label for="contact_email"><?php echo ucfirst($this->lang->line('email'))?>:</label>
+		<input type="email" name="email" id="contact_email" placeholder="<?php echo ucfirst($this->lang->line('email'))?>" class="form-control"/>
 	</div>
 	<div class="form-group">
-		<label for="contact_message">Message:</label>
-		<textarea class="form-control" rows="5" placeholder="Enter your message here!"></textarea>
+		<label for="contact_message"><?php echo ucfirst($this->lang->line('message'))?>:</label>
+		<textarea class="form-control" rows="5" placeholder="<?php echo ucfirst($this->lang->line('enter_message'))?>"></textarea>
 	</div>
-	<button type="submit" id="submit_button" class="btn btn_default">Submit</button>
+	<button type="submit" id="submit_button" class="btn btn_default"><?php echo ucfirst($this->lang->line('submit'))?></button>
 </form>
--- system/language/english/default_lang.php
+++ system/language/english/default_lang.php
@@ -16,5 +16,14 @@
 	$lang['year'] = "Year";
 	$lang['pieces_instructions'] = "Click on a title to learn more";
 
+	// Contact page stuff
+	$lang['contact_page_title'] = "Have something to say? We’d love to hear from you.";
+	$lang['contact_page_text'] = "We welcome feedback, piece suggestions, general queries, and contributions! Do leave us a note and we will get back to you as soon as possible.";
+	$lang['name'] = "name";
+	$lang['email'] = "email";
+	$lang['message'] = "message";
+	$lang['submit'] = "submit";
+	$lang['enter_message'] = "Enter your message here!";
+
 	// Error messages
 	$lang['no_results_found'] = "No results found";
