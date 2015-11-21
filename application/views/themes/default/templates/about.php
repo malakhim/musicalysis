3600311f6f68ca98df6ced01327b145b42a8da97
Mary-Anne Ng <maryanne.nghc@gmail.com>
2015-11-21 18:01:41 +1100

    testing responsive

--- application/views/themes/default/templates/about.php
+++ application/views/themes/default/templates/about.php
@@ -3,7 +3,7 @@
 <div class="about_main_container about">
 	<div class="about_text">
 	<div class="devonshire_heading">Real-time programme notes<br>made just for you.</div>
-	<div class="about_body_text">Musicalysis aims to challenge the stereotypes that Classical music is complicated and boring.<br>
+	<div class="about_body_text">Musicalysis aims to challenge the stereotypes that Classical music is complicated&nbsp;and&nbsp;boring.<br>
 	Were you always curious about classical music but felt too intimidated by the massive amount of jargon and theory? Or maybe you recognize famous tunes but not so much the rest of the piece, and would like to change that. Or perhaps you love listening to classical music and would like to delve more into its traditions.</div>
 	<div class="red_highlight_text">We love classical music, and would like to share this love with you.</div>
 	<div class="about_body_text">We decided that the most enjoyable and practical way to make classical music more engaging to everyone would simply be through listening to it - with real-time programme notes. So sit back, relax and enjoy the music.</div>
